f910ef97b41d880ec8034fa48d329bd4c9b9a36f
Kooreis <kooreis.official@gmail.com>
2024-11-29 07:48:08 -0600

    This is the start of the getMaxArea function. It initializes a stack to keep track of the indices of the bars in the histogram, and variables to keep track of the maximum area, the top of the stack, the area with the top bar, and the current index.

--- solution.java
+++ solution.java
@@ -1,7 +1,6 @@
-import java.util.Stack;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] histogram = {6, 2, 5, 4, 5, 1, 6};
-        System.out.println("Maximum area is " + getMaxArea(histogram, histogram.length));
-    }
+static int getMaxArea(int hist[], int n) {
+        Stack<Integer> s = new Stack<>();
+        int max_area = 0;
+        int tp;
+        int area_with_top;
+        int i = 0;
